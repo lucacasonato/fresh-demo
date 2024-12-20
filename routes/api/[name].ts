6959b35ffe9ad5d62acf52655c1ed71f207a0552
Luca Casonato <hello@lcas.dev>
2024-12-20 15:20:15 +0100

    test

--- routes/api/[name].ts
+++ routes/api/[name].ts
@@ -4,8 +4,15 @@ const cache = await caches.open("test");
 
 export const handler = define.handlers({
   async GET(ctx) {
-    const resp = new Response(ctx.params.name);
-    await cache.put("https://example.com", resp);
-    return new Response("cached");
+    const url = new URL("https://example.com");
+    url.searchParams.set("name", ctx.params.name);
+
+    if (ctx.url.searchParams.get("set")) {
+      await cache.put(url, new Response(ctx.url.searchParams.get("set")));
+      return new Response("cached");
+    }
+
+    const cached = await cache.match(url);
+    return cached ?? new Response("not cached");
   },
 });
